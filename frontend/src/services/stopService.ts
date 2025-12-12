501e2a2e65483055ad507d3739fb362f0b6e0850
Joona Mäki-Korte <joonamkk@gmail.com>
2025-12-12 13:29:24 +0200

    Stop liveboard base

--- frontend/src/services/stopService.ts
+++ frontend/src/services/stopService.ts
@@ -24,5 +24,18 @@ export default {
   // DELETE /stops/watchlist/{gtfs_id}
   async deleteStopWatchlistEntry(gtfs_id: string): Promise<void> {
     await apiClient.delete(`/stops/watchlist/${gtfs_id}`);
+  },
+
+  // GET /stops/live-board
+  async getLiveBoard(stops: StopWatchlist[]): Promise<StopTimetable[]> {
+    // Extract gtfs_ids
+    const idsString = stops.map(s => s.gtfs_id).join(',');
+    
+    const response = await apiClient.get<StopTimetable[]>('/stops/live-board', {
+    params: {
+      gtfs_ids: idsString 
+    }
+    });
+    return response.data;
   }
 }
